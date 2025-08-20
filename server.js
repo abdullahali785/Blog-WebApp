de80cf90cebfca1bbe854f727a45bfd14b5d5ab0
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-08-20 14:55:06 +0500

    port updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from "path";
 import methodOverride from "method-override";
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
